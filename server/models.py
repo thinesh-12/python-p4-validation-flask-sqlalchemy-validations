3d2a9194ec6e22772e5d42d2c324bd0c94b6b461
= <=>
2023-09-17 21:46:15 -0400

    completed lab

--- server/models.py
+++ server/models.py
@@ -1,5 +1,13 @@
+from email.mime import base
+from flask import session
+import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import CheckConstraint
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
 class EmailAddress(db.Model):
@@ -14,3 +22,18 @@ class EmailAddress(db.Model):
         if '@' not in address:
             raise ValueError("Failed simple email validation")
         return address
+
+Session = sessionmaker(db)
+session - Session()
+
+base.metadata.create_all(db)
+
+email = EmailAddress(email ='banana')
+session.add(email)
+
+try:
+    session.commit()
+except sqlalchemy.exec.IntegrityError as e:
+    print("Integrity violation blocked!")
+    session.rollback()
+    
